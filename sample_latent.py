23a6171786045bf6402b0cc2241b12bde55b53ea
Petr Smirnov <petr.smirnov@mail.utoronto.ca>
2017-04-13 20:27:57 -0400

    last commit before sub

--- sample_latent.py
+++ sample_latent.py
@@ -2,96 +2,74 @@ from __future__ import print_function
 
 import argparse
 import os, sys
-import h5py
 import numpy as np
-
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
-
 
 from molecules.model import MoleculeVAE
 from molecules.utils import one_hot_array, one_hot_index, from_one_hot_array, \
     decode_smiles_from_indexes, load_dataset
 
-import matplotlib 
-matplotlib.use('Agg')
+from sklearn.decomposition import PCA
 
-from pylab import figure, axes, scatter, title, show, savefig
+import matplotlib.pyplot as plt
 
-#from rdkit import Chem
-#from rdkit.Chem import Draw
-
-from keras.models import Sequential, Model, load_model
 
 LATENT_DIM = 292
 PCA_COMPONENTS = 50
-TSNE_LEARNING_RATE = 750.0
-TSNE_ITERATIONS = 1000
-TSNE_COMPONENTS = 2
-TSNE_PERPLEXITY = 30.0
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Molecular autoencoder network')
     parser.add_argument('data', type=str, help='HDF5 file to read input data from.')
     parser.add_argument('model', type=str, help='Trained Keras model to use.')
-    parser.add_argument('--save_h5', type=str, help='Name of a file to write HDF5 output to.')
     parser.add_argument('--latent_dim', type=int, metavar='N', default=LATENT_DIM,
                         help='Dimensionality of the latent representation.')
-    parser.add_argument('--tsne_lr', metavar='LR', type=float, default=TSNE_LEARNING_RATE,
-                        help='Learning to use for t-SNE.')
-    parser.add_argument('--tsne_components', metavar='N', type=int, default=TSNE_COMPONENTS,
-                        help='Number of components to use for t-SNE.')
-    parser.add_argument('--tsne_perplexity', metavar='P', type=float, default=TSNE_PERPLEXITY)
-    parser.add_argument('--tsne_iterations', metavar='N', type=int, default=TSNE_ITERATIONS)
-    parser.add_argument('--visualize', dest='visualize', action='store_true',
-                        help='Fit manifold and render a visualization. If this flag is not used, the sampled data' +
-                        ' will simply be returned with no further processing.')
-    parser.add_argument('--skip-pca', dest='use_pca', action='store_false',
-                        help='Skip PCA preprocessing of data to feed into t-SNE.')
-    parser.add_argument('--pca_components', metavar='N', type=int, default=PCA_COMPONENTS,
-                        help='Number of components to use for PCA.')
-    parser.add_argument('--v_file', type=str, help='file to save visualization in')
-    parser.set_defaults(use_pca = True)
-    parser.set_defaults(visualize = False)
+    parser.add_argument('--save_location', type=str, metavar='N', default='',
+                        help='File path where to save the plot.')
+    parser.add_argument('--pca_components', type=int, metavar='N', default=PCA_COMPONENTS,
+                        help='Number of PCA components to use')
+    parser.add_argument('--color_bar', dest='color_bar', action='store_true',
+                        help='If this flag is used, a vertical color bar will be included in the plot.')
+    parser.set_defaults(color_bar=True)
 
     return parser.parse_args()
 
-def visualize_latent_rep(args, model, x_latent, properties):
-    print("pca_on=%r pca_comp=%d tsne_comp=%d tsne_perplexity=%f tsne_lr=%f" % (
-        args.use_pca,
-        args.pca_components,
-        args.tsne_components,
-        args.tsne_perplexity,
-        args.tsne_lr
-    ))
 
-    if args.use_pca:
-        print("Computing PCA")
-        pca = PCA(n_components = args.pca_components)
-        x_latent = pca.fit_transform(x_latent)
+def visualize_latent_rep(args, x_latent, properties):
+    print("Computing PCA")
+    pca = PCA(n_components = args.pca_components)
+    x_latent = pca.fit_transform(x_latent)
 
-    print(x_latent.shape)
-    figure(figsize=(6, 6))
-    scatter(x_latent[:, 0], x_latent[:, 1], c=properties, marker='.', s=0.1)
-    if args.v_file:
-      savefig(args.v_file + '_pca.pdf', bbox_inches='tight')
-    else: 
-      show()
+    if args.save_location == '':
+        figs_path = 'figs/' + args.model.split('/')[-1].split('.h5')[0] + '.pdf'
+    else:
+        if not args.save_location.endswith('.pdf'):
+            figs_path = args.save_location + '.pdf'
+        else:
+            figs_path = args.save_location
 
-    #tsne = TSNE(n_components = args.tsne_components,
-    #            perplexity = args.tsne_perplexity,
-    #            learning_rate = args.tsne_lr,
-    #            n_iter = args.tsne_iterations,
-    #            verbose = 4)
-    #x_latent_proj = tsne.fit_transform(x_latent)
-    #del x_latent
+    directory = os.path.dirname(figs_path)
 
-    #figure(figsize=(6, 6))
-    #scatter(x_latent_proj[:, 0], x_latent_proj[:, 1], c=properties, marker='.')
-    #if args.v_file:
-    #  savefig(args.v_file + '_tsne.pdf', bbox_inches='tight')
-    #else: 
-    #  show()
+    #try:
+    #    os.stat(directory)
+    #except:
+    #    os.mkdir(directory)
+
+    if args.color_bar:
+        fig = plt.figure(figsize=(7, 6))
+    else:
+        fig = plt.figure(figsize=(6, 6))
+
+    cax = plt.scatter(x_latent[:, 0], x_latent[:, 1], c=properties, marker='.', s=0.1)
+
+    if args.color_bar:
+        cbar = fig.colorbar(cax, ticks=[np.amin(properties), np.amax(properties)])
+        cbar.ax.set_yticklabels([str(np.amin(properties)), str(np.amax(properties))])
+
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
+    plt.savefig(figs_path, bbox_inches='tight')
+    plt.show()
+
 
 def main():
     args = get_arguments()
@@ -105,16 +83,8 @@ def main():
         raise ValueError("Model file %s doesn't exist" % args.model)
 
     x_latent = model.encoder.predict(data_test)
-    if not args.visualize:
-        if not args.save_h5:
-            np.savetxt(sys.stdout, x_latent, delimiter = '\t')
-        else:
-            h5f = h5py.File(args.save_h5, 'w')
-            h5f.create_dataset('charset', data = charset)
-            h5f.create_dataset('latent_vectors', data = x_latent)
-            h5f.close()
-    else:
-        visualize_latent_rep(args, model, x_latent, properties_test)
+
+    visualize_latent_rep(args, x_latent, properties_test)
 
 if __name__ == '__main__':
     main()
